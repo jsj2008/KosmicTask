7d4f13e7eaad074ffb5c4ded0d8ae10f10fd15d4
Jonathan Mitchell <jonathan@mugginsoft.com>
2012-04-11 14:06:08 +0100

    Version 1.2.2

--- KosmicTask/KosmicTask/source/MGSNetSocket.h
+++ KosmicTask/KosmicTask/source/MGSNetSocket.h
@@ -55,7 +55,7 @@ extern NSString *const MGSNetSocketException;
 	NSUInteger _flags;
 }
 
-@property MGSNetRequest *netRequest;
+@property (assign) MGSNetRequest *netRequest;
 @property MGSAsyncSocket *socket;
 
 
