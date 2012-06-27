778f61aa1f4a28800856491e5cdaa23903b5aef8
Jonathan Mitchell <jonathan@mugginsoft.com>
2012-06-27 17:52:00 +0100

    Bumped version to 1.2.4 (67)

---
+++ KosmicTask/KosmicTask/source/NSScrollView+Mugginsoft.h
@@ -0,0 +1,13 @@
+//
+//  NSScrollView+Mugginsoft.h
+//  KosmicTask
+//
+//  Created by Mitchell Jonathan on 26/06/2012.
+//  Copyright (c) 2012 Mugginsoft. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NSScrollView (Mugginsoft)
+
+@end
---
+++ KosmicTask/KosmicTask/source/NSScrollView+Mugginsoft.m
@@ -0,0 +1,13 @@
+//
+//  NSScrollView+Mugginsoft.m
+//  KosmicTask
+//
+//  Created by Mitchell Jonathan on 26/06/2012.
+//  Copyright (c) 2012 Mugginsoft. All rights reserved.
+//
+
+#import "NSScrollView+Mugginsoft.h"
+
+@implementation NSScrollView (Mugginsoft)
+
+@end
