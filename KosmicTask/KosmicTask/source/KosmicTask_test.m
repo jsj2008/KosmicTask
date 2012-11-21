4213510281ef8559ea0d731f6e3a57f30219bd95
Jonathan Mitchell <jonathan@mugginsoft.com>
2012-11-21 09:26:08 +0000

    Added some string category unit tests

--- KosmicTask/KosmicTask/source/KosmicTask_test.m
+++ KosmicTask/KosmicTask/source/KosmicTask_test.m
@@ -8,6 +8,7 @@
 
 #import "KosmicTask_test.h"
 #import "KosmicTaskController.h"
+#import "NSString_Mugginsoft.h"
 
 /*
  common macros defined in SenTestingKit.h
@@ -39,5 +40,37 @@
     NSString *fourChar = @"PsOf";
     NSInteger val = [KosmicTaskController fourCharToInteger:fourChar];
     STAssertEquals(val, 1349734246, @"%@ Four char string to integer", fourChar);
+}
+
+- (void)testStringCategories
+{
+    // UUID
+    STAssertTrue([@"DB00EC6E-F96D-4CA4-B2E9-B8AB4324B791" mgs_isUUID], @"valid UUID");
+    
+    STAssertFalse([@"DB00EC6E_F96D-4CA4-B2E9-B8AB4324B791" mgs_isUUID], @"invalid UUID");
+    STAssertFalse([@"D00EC6E-F96D-4CA4-B2E9-B8AB4324B791" mgs_isUUID], @"invalid UUID");
+    STAssertFalse([@"D00EC6E-F96-4CA4-B2E9-B8AB4324B791" mgs_isUUID], @"invalid UUID");
+    STAssertFalse([@"D00EC6E-F96D-4CA4-B29-B8AB4324B791" mgs_isUUID], @"invalid UUID");
+    STAssertFalse([@"D00EC6E-F96D-4CA4-B299-8AB4324B791" mgs_isUUID], @"invalid UUID");
+    STAssertFalse([@"D0$EC6E-F96D-4CA4-B299-8AB4324B791" mgs_isUUID], @"invalid UUID");
+    
+    // URL
+    STAssertTrue([@"http://www.mugginsoft.com" mgs_isURL], @"valid URL");
+    STAssertTrue([@"https://www.mugginsoft.com" mgs_isURL], @"valid URL");
+    STAssertTrue([@"www.mugginsoft.com" mgs_isURL], @"valid URL");
+    STAssertTrue([@"mugginsoft.com" mgs_isURL], @"valid URL");
+    STAssertTrue([@"mugginsoft.museum" mgs_isURL], @"valid URL");
+
+    STAssertFalse([@"http://www.mugginsoft.c" mgs_isURL], @"invalid URL");
+    STAssertFalse([@"http://#www.mugginsoft.com" mgs_isURL], @"invalid URL");
+    
+    // IP address
+    STAssertTrue([@"192.168.0.23" mgs_isIPAddress], @"valid IP");
+    STAssertTrue([@"fe80::cabc:c8ff:fea5:d5db" mgs_isIPAddress], @"valid IP");
+
+    STAssertFalse([@"192.168.o.23" mgs_isIPAddress], @"invalid IP");
+    STAssertFalse([@"192.168.023" mgs_isIPAddress], @"invalid IP");
+    STAssertFalse([@"fe80:cabc:c8ff:fea5:d5db" mgs_isIPAddress], @"invalid IP");
+    STAssertFalse([@"fe8o:cabc:c8ff:fea5:d5db" mgs_isIPAddress], @"invalid IP");
 }
 @end
