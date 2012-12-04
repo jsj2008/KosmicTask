69fd2f605bd26b67c06718df18b9e8d4a073e88b
Jonathan Mitchell <jonathan@mugginsoft.com>
2012-12-04 15:25:24 +0000

    Fragaria updated

---
+++ KosmicTask/KosmicTask/source/MGSAppResources.h
@@ -0,0 +1,13 @@
+//
+//  MGSResources.h
+//  KosmicTask
+//
+//  Created by Jonathan on 04/12/2012.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MGSResources : NSObject
+
+@end
---
+++ KosmicTask/KosmicTask/source/MGSAppResources.m
@@ -0,0 +1,13 @@
+//
+//  MGSResources.m
+//  KosmicTask
+//
+//  Created by Jonathan on 04/12/2012.
+//
+//
+
+#import "MGSResources.h"
+
+@implementation MGSResources
+
+@end
