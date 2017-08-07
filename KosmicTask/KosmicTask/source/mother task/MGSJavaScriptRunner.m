f6ca1a594ba809d6ecb80d01efee4f5df800a5c1
Jonathan Mitchell <jonathan@jonathans-mac-3.home>
2017-08-07 12:39:55 +0100

    JavaScript runner converted to ARC

--- KosmicTask/KosmicTask/source/mother task/MGSJavaScriptRunner.m
+++ KosmicTask/KosmicTask/source/mother task/MGSJavaScriptRunner.m
@@ -70,125 +70,143 @@ JSValueRef MGSJSLogFunction(JSContextRef ctx, JSObjectRef function, JSObjectRef 
 	
 	// JSCocoa has all this wrapped!
 	
-	NSString *taskFunction = self.runFunctionName;
-	NSString *resultString = @"";
-	JSStringRef scriptJS = NULL;
-	
-	// get script parameter array
-	NSArray *paramArray = [self ScriptParametersWithError:YES];
-	if (!paramArray) return NO;
+    // setup JS
+    JSGlobalContextRef ctx = JSGlobalContextCreate(NULL);
+    JSObjectRef globalJS = JSContextGetGlobalObject(ctx);
+    JSValueRef exception = NULL;
+    JSObjectRef fn = NULL;
+    JSStringRef scriptJS = NULL;
+    JSValueRef *arguments = NULL;
+    NSString *resultString = @"";
 
-	// get the executable source
-	NSString *source = [self scriptExecutableSourceWithError:YES];
-	if (!source) {
-		return NO;
-	}
-	
-	// setup JS
-	JSGlobalContextRef ctx = JSGlobalContextCreate(NULL);
-	JSObjectRef globalJS = JSContextGetGlobalObject(ctx);
-	JSValueRef exception = NULL;
-	JSObjectRef fn = NULL;
+    @try {
+        NSString *taskFunction = self.runFunctionName;
+        
+        // get script parameter array
+        NSArray *paramArray = [self ScriptParametersWithError:YES];
+        if (!paramArray) return NO;
 
-	// extend the context
-	[self extendContext:ctx];
-		
-	// load the KosmicTaskController source
-	NSString *controllerPath = [self pathToResource:@"JavaScript/KosmicTaskController.js"];
-	NSString *controllerSource = [NSString stringWithContentsOfFile:controllerPath encoding:NSUTF8StringEncoding error:NULL];
-	if (!controllerSource) {
-		self.error = [NSString stringWithFormat:@"Cannot load KosmicTaskController : %@", controllerPath];
-		return NO;
-	}
-	
-	// evaluate the controller within the global context
-	JSStringRef controllerJS = MGSJSStringRefCreateWithNSString(controllerSource);
-	JSStringRef controllerURLJS = MGSJSStringRefCreateWithNSString(@"KosmicTaskController");
-	JSEvaluateScript(ctx, controllerJS, NULL, controllerURLJS, 1, &exception);
-	if (exception) goto handleException;
-	
-	// load the task script
-	scriptJS = MGSJSStringRefCreateWithNSString(source);
-	JSStringRef sourceURLJS = MGSJSStringRefCreateWithNSString(@"KosmicTask");
-	
-	// evaluate our script for the global object.
-	// if we define a task function then that task function will become available
-	// as a property on the global object.
-	// we can then execute an anonymous function with arguments to call our
-	// task function.
-	// or we can get the property for our function and execute it
-	JSValueRef resultJS = JSEvaluateScript(ctx, scriptJS, NULL, sourceURLJS, 1, &exception);
-	if (exception) goto handleException;
+        // get the executable source
+        NSString *source = [self scriptExecutableSourceWithError:YES];
+        if (!source) {
+            return NO;
+        }
 
-	// property approach.
-	// functions are stored as properties of, in this case, the global object.
-	
-	// get property
-	JSStringRef propertyName = MGSJSStringRefCreateWithNSString(taskFunction);
-	fn = (JSObjectRef)JSObjectGetProperty(ctx, globalJS, propertyName, &exception);
-	if (exception) goto handleException;
-	
-	// validate
-	if (!JSObjectIsFunction(ctx, fn)) {
-		self.error = [NSString stringWithFormat:@"Function not found: %@", taskFunction];
-		goto handleException;
-	}
-	
-	// form argument list
-	size_t argumentCount = [paramArray count];
-	JSValueRef *arguments = NULL;
-	if (argumentCount > 0) {
-		arguments = NSAllocateCollectable(sizeof(JSValueRef) * argumentCount, 0);
-		for (NSUInteger i = 0; i < argumentCount; i++ ) {
-			
-			JSValueRef arg = NULL;
-			
-			// get parameter and form appropriate JS type
-			id param = [paramArray objectAtIndex:i];
-			
-			// number
-			if ([param isKindOfClass:[NSNumber class]]) {
-				arg = JSValueMakeNumber(ctx, [param doubleValue]);
-			} else {
-				JSStringRef stringRep = MGSJSStringRefCreateWithNSString([param description]);
-				arg = JSValueMakeString(ctx, stringRep);
-			}
-			
-			arguments[i] = arg;
-		}
-	}
-	
-	if (fn) {
-		
-		// call the function
-		resultJS = JSObjectCallAsFunction(ctx, fn, NULL, argumentCount, arguments, &exception);
-		if (exception) goto handleException;
-		
-		// get result as string
-		JSStringRef resultStringJS = JSValueToStringCopy(ctx, resultJS, &exception);
-		if (exception) goto handleException;
-		resultString = MGSStringCreateWithJSString(resultStringJS);
-		
-	} else {
-		self.error = [NSString stringWithFormat:@"Function missing: %@", taskFunction];
-	}
+        // extend the context
+        [self extendContext:ctx];
+            
+        // load the KosmicTaskController source
+        NSString *controllerPath = [self pathToResource:@"JavaScript/KosmicTaskController.js"];
+        NSString *controllerSource = [NSString stringWithContentsOfFile:controllerPath encoding:NSUTF8StringEncoding error:NULL];
+        if (!controllerSource) {
+            self.error = [NSString stringWithFormat:@"Cannot load KosmicTaskController : %@", controllerPath];
+            return NO;
+        }
+        
+        // evaluate the controller within the global context
+        JSStringRef controllerJS = MGSJSStringRefCreateWithNSString(controllerSource);
+        JSStringRef controllerURLJS = MGSJSStringRefCreateWithNSString(@"KosmicTaskController");
+        JSEvaluateScript(ctx, controllerJS, NULL, controllerURLJS, 1, &exception);
+        if (exception) {
+            [NSException raise:MGSScriptRunnerBuildException format:@""];
+        }
+        
+        // load the task script
+        scriptJS = MGSJSStringRefCreateWithNSString(source);
+        JSStringRef sourceURLJS = MGSJSStringRefCreateWithNSString(@"KosmicTask");
+        
+        // evaluate our script for the global object.
+        // if we define a task function then that task function will become available
+        // as a property on the global object.
+        // we can then execute an anonymous function with arguments to call our
+        // task function.
+        // or we can get the property for our function and execute it
+        JSValueRef resultJS = JSEvaluateScript(ctx, scriptJS, NULL, sourceURLJS, 1, &exception);
+        if (exception) {
+            [NSException raise:MGSScriptRunnerBuildException format:@""];
+        }
 
-handleException:
-	
-	// handle exception
-	if (exception) {
-		JSStringRef exceptionString = JSValueToStringCopy(ctx, exception, NULL);
-		self.error = MGSStringCreateWithJSString(exceptionString);
-		JSStringRelease(exceptionString);	
-	}
-	
-	// clean up
-	if (scriptJS) {
-		JSStringRelease(scriptJS);	
-	}
-	JSGlobalContextRelease(ctx);	
-	
-	return [self processExecuteResult:resultString];
+        // property approach.
+        // functions are stored as properties of, in this case, the global object.
+        
+        // get property
+        JSStringRef propertyName = MGSJSStringRefCreateWithNSString(taskFunction);
+        fn = (JSObjectRef)JSObjectGetProperty(ctx, globalJS, propertyName, &exception);
+        if (exception) {
+            [NSException raise:MGSScriptRunnerBuildException format:@""];
+        }
+
+        // validate
+        if (!JSObjectIsFunction(ctx, fn)) {
+            self.error = [NSString stringWithFormat:@"Function not found: %@", taskFunction];
+            [NSException raise:MGSScriptRunnerBuildException format:@"%@", self.error];
+        }
+        
+        // form argument list
+        size_t argumentCount = [paramArray count];
+        if (argumentCount > 0) {
+            arguments = malloc(sizeof(JSValueRef) * argumentCount);
+            for (NSUInteger i = 0; i < argumentCount; i++ ) {
+                
+                JSValueRef arg = NULL;
+                
+                // get parameter and form appropriate JS type
+                id param = [paramArray objectAtIndex:i];
+                
+                // number
+                if ([param isKindOfClass:[NSNumber class]]) {
+                    arg = JSValueMakeNumber(ctx, [param doubleValue]);
+                } else {
+                    JSStringRef stringRep = MGSJSStringRefCreateWithNSString([param description]);
+                    arg = JSValueMakeString(ctx, stringRep);
+                }
+                
+                arguments[i] = arg;
+            }
+        }
+        
+        if (fn) {
+            
+            // call the function
+            resultJS = JSObjectCallAsFunction(ctx, fn, NULL, argumentCount, arguments, &exception);
+            if (exception) {
+                [NSException raise:MGSScriptRunnerBuildException format:@""];
+            }
+            
+            // get result as string
+            JSStringRef resultStringJS = JSValueToStringCopy(ctx, resultJS, &exception);
+            if (exception) {
+                [NSException raise:MGSScriptRunnerBuildException format:@""];
+            }
+            resultString = MGSStringCreateWithJSString(resultStringJS);
+            
+        } else {
+            self.error = [NSString stringWithFormat:@"Function missing: %@", taskFunction];
+        }
+        
+    }
+    @catch (NSException *e) {
+        
+        // handle exception
+        if (exception) {
+            JSStringRef exceptionString = JSValueToStringCopy(ctx, exception, NULL);
+            self.error = MGSStringCreateWithJSString(exceptionString);
+            JSStringRelease(exceptionString);	
+        }
+    }
+    @finally {
+        
+        // clean up
+        if (scriptJS) {
+            JSStringRelease(scriptJS);
+        }
+        if (arguments != NULL) {
+            free(arguments);
+        }
+        JSGlobalContextRelease(ctx);
+        
+        return [self processExecuteResult:resultString];
+    }
 }
 
 /*
